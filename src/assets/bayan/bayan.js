e3b171b27a2680506a3e9e25621e1fac8e8ae4bb
Boffeer <41575886+Boffeer@users.noreply.github.com>
2022-03-24 10:58:15 +0300

    update bayan

--- src/assets/bayan/bayan.js
+++ src/assets/bayan/bayan.js
@@ -5,18 +5,19 @@ const bayans = [...document.querySelectorAll(".bayan")];
 const bayanOpenedClass = "bayan--opened";
 const bayanHeight = 1000;
 
-function toggleBayan(
-  bayanTop,
-  bayanBottom,
-  bayanOpenedClass,
-  bayanBottomHeight
-) {
-  if (bayanTop.parentElement.classList.value.includes(bayanOpenedClass)) {
-    bayanBottom.style.maxHeight = "0";
-    bayanTop.parentElement.classList.remove(bayanOpenedClass);
+function toggleBayan(bayanObject) {
+  if (!bayanObject) return;
+
+  if (
+    bayanObject.top.bayan.parentElement.classList.value.includes(
+      bayanOpenedClass
+    )
+  ) {
+    bayanObject.bottom.bayan.style.maxHeight = "0";
+    bayanObject.top.bayan.parentElement.classList.remove(bayanOpenedClass);
   } else {
-    bayanBottom.style.maxHeight = `${bayanBottomHeight}px`;
-    bayanTop.parentElement.classList.add(bayanOpenedClass);
+    bayanObject.bottom.bayan.style.maxHeight = `${bayanHeight}px`;
+    bayanObject.top.bayan.parentElement.classList.add(bayanOpenedClass);
   }
 }
 
@@ -24,46 +25,51 @@ if (bayans.length > 0) {
   bayans.forEach((bayan, index) => {
     let [bayanTopContent, bayanBottomContent] = Array.from(bayan.children);
 
-    function createBayanStructure(bayan) {
+    let bayanObject = {
+      top: {
+        content: bayanTopContent,
+      },
+      bottom: {
+        content: bayanBottomContent,
+      },
+      bayan,
+    };
+
+    function createBayanStructure(bayanObject) {
       let bayanTop = document.createElement("div");
       bayanTop.classList.add("bayan__top");
       bayanTop.appendChild(bayanTopContent);
-      bayan.appendChild(bayanTop);
+      bayanObject.bayan.appendChild(bayanTop);
+      bayanObject.top.bayan = bayanTop;
 
       let bayanBottom = document.createElement("div");
       bayanBottom.classList.add("bayan__bottom");
       bayanBottom.appendChild(bayanBottomContent);
-      bayan.appendChild(bayanBottom);
+      bayanObject.bayan.appendChild(bayanBottom);
+      bayanObject.bottom.bayan = bayanBottom;
 
-      function toggleBayanShorthand() {
-        toggleBayan(bayanTop, bayanBottom, bayanOpenedClass, bayanHeight);
-      }
-      bayanTop.addEventListener("click", () => toggleBayanShorthand());
+      bayanTop.addEventListener("click", () => toggleBayan(bayanObject));
 
-      if (bayan.getAttribute("data-bayan")?.includes("absolute")) {
-        bayanBottom.style.position = "absolute";
+      if (bayanObject.bayan.getAttribute("data-bayan")?.includes("absolute")) {
+        bayanObject.bottom.bayan.style.position = "absolute";
       }
-      setTimeout(() => {
-        bayanTop.click();
-        toggleBayanShorthand();
-      }, 200);
     }
 
-    if (bayan.classList.value.includes("bayan--clones")) {
-      let bayanNoClick = document.createElement("div");
-      bayanNoClick.classList.add("bayan--static");
-      bayan.appendChild(bayanNoClick);
-      bayanNoClick.appendChild(bayanTopContent.cloneNode(true));
-      bayanNoClick.appendChild(bayanBottomContent.cloneNode(true));
+    if (bayanObject.bayan.classList.value.includes("bayan--clones")) {
+      let bayanStatic = document.createElement("div");
+      bayanStatic.classList.add("bayan--static");
+      bayanObject.bayan.appendChild(bayanStatic);
+      bayanStatic.appendChild(bayanObject.top.content.cloneNode(true));
+      bayanStatic.appendChild(bayanObject.bottom.content.cloneNode(true));
 
-      let bayanClickable = document.createElement("div");
-      bayanClickable.classList.add("bayan--dynamic");
-      bayan.appendChild(bayanClickable);
-      createBayanStructure(bayanClickable);
-
-      let noBayan = bayanClickable.cloneNode(true);
+      let bayanDynamyc = document.createElement("div");
+      bayanDynamyc.classList.add("bayan--dynamic");
+      bayanObject.bayan.appendChild(bayanDynamyc);
+      createBayanStructure(bayanObject);
+      bayanDynamyc.appendChild(bayanObject.top.bayan);
+      bayanDynamyc.appendChild(bayanObject.bottom.bayan);
     } else {
-      createBayanStructure(bayan);
+      createBayanStructure(bayanObject);
     }
   });
 }
